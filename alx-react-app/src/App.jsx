91306470e1355d9dc050fba94ea6acef3856a659
Saadia Nazih <saadia.nazih92@gmail.com>
2025-10-30 18:48:53 +0100

    update welcome file

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import WelcomeMessage from './WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
