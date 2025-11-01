8778c7fad9f56b396ed51a9fbf9e5d45cdec5851
Saadia Nazih <saadia.nazih92@gmail.com>
2025-11-01 19:43:25 +0100

    new update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Footer'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -13,20 +14,10 @@ function App() {
   return (
     <>
      <WelcomeMessage />
-     <Header />
+     <Header name="Alice" age="25" bio="Loves hiking and photography"/>
      <MainContent />
      <Footer />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      
-     
+    <UserProfile /> 
     </>
   )
 }
