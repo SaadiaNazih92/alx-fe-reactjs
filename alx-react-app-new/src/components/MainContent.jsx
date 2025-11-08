4a99d3321b129680cc0ce16ae052b9ca3059eeb9
Saadia Nazih <saadia.nazih92@gmail.com>
2025-11-08 01:46:04 -0800

    initial commit

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function MainContent() {
+  const mainStyle = {
+    padding: '20px',
+    backgroundColor: '#f4f4f4' 
+  };
+
+  return (
+    <main style={mainStyle}>
+      <p>Here is my list of favorite cities...</p>
+    </main>
+  );
+}
+
+export default MainContent;
