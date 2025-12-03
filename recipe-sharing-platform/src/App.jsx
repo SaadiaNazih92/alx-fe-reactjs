ec74646150de9b54ce9233e5bfe7ae674b05e5be
Saadia Nazih <saadia.nazih92@gmail.com>
2025-12-03 19:34:13 +0100

    added new file

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import RecipeDetail from './components/RecipeDetail'; // Non preoccuparti se dà errore ora
+import RecipeDetail from './components/RecipeDetail';
+import AddRecipeForm from './components/AddRecipeForm'; // <--- Importa il nuovo componente
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/recipe/:id" element={<RecipeDetail />} />
+          <Route path="/add-recipe" element={<AddRecipeForm />} /> {/* <--- Nuova rotta */}
         </Routes>
       </div>
     </Router>
