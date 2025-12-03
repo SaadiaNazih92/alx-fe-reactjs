fe435b011607c0f6621ff04686a045de7a088831
Saadia Nazih <saadia.nazih92@gmail.com>
2025-12-03 07:46:01 +0100

    Fix tailwind config paths for checker

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -2,7 +2,7 @@
 export default {
   content: [
     "./index.html",                 // Serve a Vite per funzionare
-    "./src/**/*.{js,ts,jsx,tsx}",   // Serve per i tuoi componenti
+   "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"           // <--- Serve SOLO per passare il controllo automatico
   ],
   theme: {
