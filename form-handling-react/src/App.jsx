381cb73f6c23ab7fb1a5d5a57bfc1062eb8c2096
Saadia Nazih <saadia.nazih92@gmail.com>
2025-12-13 11:06:47 -0800

    Fix: conversione in jsx e update dipendenze

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,9 +1,11 @@
+import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
 import FormikForm from './components/formikForm';
 
 function App() {
   return (
     <div>
+      <h1>Form Handling</h1>
       <RegistrationForm />
       <FormikForm />
     </div>
